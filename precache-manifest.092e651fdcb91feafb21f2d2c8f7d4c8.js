44776fb060221a6e2a162630e8df8c7d2a499d71
Leandro Faria <leandro.faria@mercadolivre.com>
2019-05-19 12:43:25 -0300

    Add files via upload

---
+++ precache-manifest.092e651fdcb91feafb21f2d2c8f7d4c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58523672068e4e984d2b71b56b412210",
+    "url": "/fiap-react-17mob/index.html"
+  },
+  {
+    "revision": "732815f509285bde9777",
+    "url": "/fiap-react-17mob/static/css/main.bb2c2c5e.chunk.css"
+  },
+  {
+    "revision": "31f1039b0a354c437db4",
+    "url": "/fiap-react-17mob/static/js/2.af8d32b8.chunk.js"
+  },
+  {
+    "revision": "732815f509285bde9777",
+    "url": "/fiap-react-17mob/static/js/main.f2e32132.chunk.js"
+  },
+  {
+    "revision": "08716792a5988e0da7ba",
+    "url": "/fiap-react-17mob/static/js/runtime~main.2f6b26ef.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.91cea46fffc7d967d134c94742a8aebe.js"
+  "/fiap-react-17mob/precache-manifest.092e651fdcb91feafb21f2d2c8f7d4c8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fiap-react-17mob/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
