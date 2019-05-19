38985e8becbbf9b7ae7f906324162ba6aaf14eed
Leandro Faria <leandro.faria@mercadolivre.com>
2019-05-19 12:25:37 -0300

    Add files via upload

---
+++ precache-manifest.91cea46fffc7d967d134c94742a8aebe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be7d783776c1e6c0d53c35038b81f3a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b29ce52ee4ad3bed0b59",
+    "url": "/static/css/main.bb2c2c5e.chunk.css"
+  },
+  {
+    "revision": "31f1039b0a354c437db4",
+    "url": "/static/js/2.af8d32b8.chunk.js"
+  },
+  {
+    "revision": "b29ce52ee4ad3bed0b59",
+    "url": "/static/js/main.67287ba5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
